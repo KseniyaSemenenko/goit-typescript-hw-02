e7e853607cb34a46d409b76bf7c62a636a97c287
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2024-11-24 18:25:30 +0200

    change loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import { RotatingLines } from 'react-loader-spinner';
 
 const Loader: React.FC = () => {
   return (
     <RotatingLines
       visible={true}
-      height="96"
+      // height="96"
       width="96"
-      color="grey"
+      // color="grey"
       strokeWidth="5"
       animationDuration="0.75"
       ariaLabel="rotating-lines-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
+      // wrapperStyle={{}}
+      // wrapperClass=""
     />
   );
 };
